35abcd8ddede898c287570688987edd2bbcabadf
ghuertas <guillaumehuertas@free.fr>
2019-05-06 16:09:14 +0200

    update de HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		// System.out.println permet d'afficher un message sur la console
 		System.out.println("Hello World !!!");
+		System.out.println("Bientôt un vrai programme");
 	}
 
 }
